6da745187f79d639b72764972ddbf329deed640f
Jarek Kajko <jaruto7@gmail.com>
2018-10-02 20:40:23 +0200

    Create little kanban with server functionality

--- js/App.js
+++ js/App.js
@@ -1,12 +1,47 @@
-// OGÓLNA FUNKCJA
-function randomString() {
-	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
-	var str = '', i;
-	for (i = 0; i < 10; i++) {
-	  str += chars[Math.floor(Math.random() * chars.length)];
-	}
-	return str;
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+    'X-Client-Id': 3239,
+    'X-Auth-Token': '4b7e36984dcfbe3af1e978f88232d161'
+};
+
+fetch( baseUrl + '/board', { headers: myHeaders } )
+    .then( function( resp ) {
+        return resp.json();
+    })
+    .then( function( resp ) {
+        setupColumns( resp. columns );
+    });
+
+function setupColumns( columns ) {
+    columns.forEach( function( column ) {
+        var col = new Column( column.id, column.name );
+        board.addColumn( col );
+        setupCards( col, column.cards );
+    });
 }
+
+function setupCards( col, cards ) {
+    cards.forEach( function( card ) {
+        var cardObj = new Card( card.id, card.name );
+        col.addCard( cardObj );
+    });
+}
+
+Column.prototype = {
+    addCard: function( card ) {
+      this.element.querySelector( 'ul' ).appendChild( card.element );
+    }
+};
+
+// OGÓLNA FUNKCJA - JUZ NIE POTRZEBNA
+// function randomString() {
+// 	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
+// 	var str = '', i;
+// 	for (i = 0; i < 10; i++) {
+// 	  str += chars[Math.floor(Math.random() * chars.length)];
+// 	}
+// 	return str;
+// }
 
 function generateTemplate(name, data, basicElement) {
   	var template = document.getElementById(name).innerHTML;
@@ -17,21 +52,3 @@ function generateTemplate(name, data, basicElement) {
   
   	return element;
 }
-
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
-var todoColumn = new Column('Do zrobienia');
-var doingColumn = new Column('W trakcie');
-var doneColumn = new Column('Skończone');
-
-// DODAWANIE KOLUMN DO TABLICY
-board.createColumn(todoColumn);
-board.createColumn(doingColumn);
-board.createColumn(doneColumn);
-
-// TWORZENIE NOWYCH EGZEMPLARZY KART
-var card1 = new Card('Nowe zadanie');
-var card2 = new Card('stworzyc tablice kanban');
-
-// DODAWANIE KART DO KOLUMN
-todoColumn.createCard(card1);
-doingColumn.createCard(card2);
